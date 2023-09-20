cd824c4d2322c0bb7a5593b681a4b6290c1d4087
silquer16 <130589974+silquer16@users.noreply.github.com>
2023-09-20 13:55:36 -0300

    .

---
+++ script.js
@@ -0,0 +1,14 @@
+// Função para abrir o modal
+function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+// Função para fechar o modal
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+// Adicione aqui o código para processar o login ou registro
+// Após o login ou registro bem-sucedido, você pode chamar closeModal() para fechar o modal automaticamente.

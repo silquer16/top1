2d63847db6c9fae948e3127eb33b6e27e5207b9a
silquer16 <130589974+silquer16@users.noreply.github.com>
2023-09-20 14:10:37 -0300

    ddd

---
+++ script.js
@@ -0,0 +1,34 @@
+// Array para armazenar os itens do carrinho
+let carrinhoItens = [];
+
+// Função para adicionar itens ao carrinho
+function adicionarAoCarrinho(nome, preco) {
+    carrinhoItens.push({ nome, preco });
+    atualizarCarrinho();
+}
+
+// Função para atualizar a exibição do carrinho
+function atualizarCarrinho() {
+    const carrinhoLista = document.getElementById("carrinho");
+    carrinhoLista.innerHTML = "";
+
+    let total = 0;
+
+    for (const item of carrinhoItens) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${item.nome} - R$ ${item.preco}`;
+        carrinhoLista.appendChild(listItem);
+
+        total += item.preco;
+    }
+
+    const totalItem = document.createElement("li");
+    totalItem.textContent = `Total: R$ ${total}`;
+    carrinhoLista.appendChild(totalItem);
+}
+
+// Função para ver o resumo do carrinho
+function verResumo() {
+    // Crie uma página separada (resumo.html) e redirecione para ela
+    window.location.href = "resumo.html";
+}
